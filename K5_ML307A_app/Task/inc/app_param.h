e59f02b913264e7be9522473e5556f9eb2dfb08a
Lyu <188436647@qq.com>
2024-04-22 16:25:39 +0800

    2.4.2的烧录hex文件丢失，基于2.4.2重新编译生成2.4.3，软件完全一样

--- K5_ML307A_app/Task/inc/app_param.h
+++ K5_ML307A_app/Task/inc/app_param.h
@@ -22,7 +22,7 @@
 #define BOOT_PARAM_FLAG         0xB0
 #define OTA_PARAM_FLAG          0x21
 
-#define EEPROM_VERSION									"K5_ML307A_HD_V2.4.2"
+#define EEPROM_VERSION									"K5_ML307A_HD_V2.4.3"
 
 
 #define JT808_PROTOCOL_TYPE			8
